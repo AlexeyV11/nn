9f6b6721629ea507ac14334cbb428fc123085fac
a.vilkin <alexey.vilkin@apical.co.uk>
2016-04-21 16:45:36 +0100

    matrix operations instead of repmat

--- fc_learning.m
+++ fc_learning.m
@@ -6,13 +6,13 @@ function [] = fc_learning
     learning_rate = 0.7;
     
     input_values = [0 0; 0 1; 1 0; 1 1];
-    target_values = [0 ; 1; 1; 0];
+    target_values = [0; 1; 1; 0];
     
     hidden_neurons_count = 2;
     rng(0,'v5uniform');
     INIT_EPISLON = 0.8;
 
-    bias = -1;
+    
 
     W_input_to_hidden = rand(size(input_values,2)+1, hidden_neurons_count) * (2*INIT_EPISLON) - INIT_EPISLON;
     assert(isequal(size(W_input_to_hidden), [size(input_values,2)+1 hidden_neurons_count]));
@@ -31,6 +31,8 @@ function [] = fc_learning
         target = target_values(ind,:);
         input = input_values(ind,:);
         
+        bias = -ones(size(input,1), 1);
+        
         % forward pass
         hidden_i = [input bias] * W_input_to_hidden; %sum(W_input_to_hidden' .* repmat([input bias], [hidden_neurons_count 1]),2);%
         hidden_o = activation(hidden_i);
@@ -44,12 +46,12 @@ function [] = fc_learning
         
         % backward pass
         De_Dotput_o = (output_o - target);
-        Do_Doutput_i = activation_der(output_i');
+        Do_Doutput_i = activation_der(output_i);
         De_Doutput_i = De_Dotput_o .* Do_Doutput_i;
         % delta_hidden=delta_output*W2.*dsigmoid(Z2);
 
-        d_W_hidden_to_output = repmat(De_Doutput_i, [numel([hidden_o bias]) 1])' .* repmat([hidden_o bias], [numel(De_Doutput_i) 1]);
-
+        d_W_hidden_to_output = De_Doutput_i' * [hidden_o bias];
+        
         dE_Dhidden_o = W_hidden_to_output * De_Doutput_i';
         do_Dhidden_i = activation_der([hidden_i bias]);
         dE_Dhidden_i = do_Dhidden_i .* dE_Dhidden_o';
@@ -57,20 +59,16 @@ function [] = fc_learning
         
         %De_Doutput_i
         %dE_Dhidden_i
-
-        d_W_input_to_hidden = repmat(dE_Dhidden_i, [numel([input bias]) 1])' .* repmat([input bias], [numel(dE_Dhidden_i) 1]);
+        d_W_input_to_hidden = dE_Dhidden_i' * [input bias];
 
         W_input_to_hidden = W_input_to_hidden - learning_rate * d_W_input_to_hidden';
         W_hidden_to_output = W_hidden_to_output - learning_rate * d_W_hidden_to_output';
 
-        %pause(0.00005);
-        
         %WWW = [W_input_to_hidden' ; W_hidden_to_output']; 
         %WWW
         
         %DW = [d_W_input_to_hidden ; d_W_hidden_to_output];
         %DW
-        
     end
     
      for ind = 1:4
