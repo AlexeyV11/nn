20f4fe06f9511b2809663ae7263395ea8ec12746
a.vilkin <alexey.vilkin@apical.co.uk>
2016-04-21 17:54:21 +0100

    vectorised implementation of backpropogation

--- fc_learning.m
+++ fc_learning.m
@@ -3,34 +3,33 @@
 function [] = fc_learning
 
     % https://mattmazur.com/2015/03/17/a-step-by-step-backpropagation-example/
-    learning_rate = 0.7;
+    learning_rate = 2.0;
     
     input_values = [0 0; 0 1; 1 0; 1 1];
     target_values = [0; 1; 1; 0];
     
     hidden_neurons_count = 2;
+    output_neurons_count = 1;
+    
     rng(0,'v5uniform');
     INIT_EPISLON = 0.8;
-
-    
 
     W_input_to_hidden = rand(size(input_values,2)+1, hidden_neurons_count) * (2*INIT_EPISLON) - INIT_EPISLON;
     assert(isequal(size(W_input_to_hidden), [size(input_values,2)+1 hidden_neurons_count]));
     
-    output_neurons_count = 1;
     W_hidden_to_output = rand(hidden_neurons_count+1, output_neurons_count) * (2*INIT_EPISLON) - INIT_EPISLON;
     assert(isequal(size(W_hidden_to_output), [hidden_neurons_count+1 output_neurons_count]));
     
-    for iter = 1:10000
+    for iter = 1:20000
         ind = rem(iter,4);
         
         if(ind == 0)
             ind = 4;
         end
         
-        target = target_values(ind,:);
-        input = input_values(ind,:);
-        
+        target = target_values;%(ind,:);
+        input = input_values;%(ind,:);
+
         bias = -ones(size(input,1), 1);
         
         % forward pass
@@ -41,7 +40,6 @@ function [] = fc_learning
         output_o = activation(output_i);
 
         e = (target - output_o) .* (target - output_o) / 2;
-
         %disp(sum(e));
         
         % backward pass
@@ -51,16 +49,18 @@ function [] = fc_learning
         % delta_hidden=delta_output*W2.*dsigmoid(Z2);
 
         d_W_hidden_to_output = De_Doutput_i' * [hidden_o bias];
+        d_W_hidden_to_output = d_W_hidden_to_output / size(target,1);
         
-        dE_Dhidden_o = W_hidden_to_output * De_Doutput_i';
+        dE_Dhidden_o = De_Doutput_i * W_hidden_to_output';
         do_Dhidden_i = activation_der([hidden_i bias]);
-        dE_Dhidden_i = do_Dhidden_i .* dE_Dhidden_o';
-        dE_Dhidden_i = dE_Dhidden_i(1:end-1);
+        dE_Dhidden_i = do_Dhidden_i .* dE_Dhidden_o;
+        dE_Dhidden_i = dE_Dhidden_i(:,1:end-1);
         
         %De_Doutput_i
         %dE_Dhidden_i
         d_W_input_to_hidden = dE_Dhidden_i' * [input bias];
-
+        d_W_input_to_hidden = d_W_input_to_hidden / size(target,1);
+        
         W_input_to_hidden = W_input_to_hidden - learning_rate * d_W_input_to_hidden';
         W_hidden_to_output = W_hidden_to_output - learning_rate * d_W_hidden_to_output';
 
@@ -71,22 +71,26 @@ function [] = fc_learning
         %DW
     end
     
-     for ind = 1:4
-        target = target_values(ind,:);
-        input = input_values(ind,:);
-        
-        % forward pass
-        hidden_i = [input bias] * W_input_to_hidden;
-        hidden_o = activation(hidden_i);
+     
+    target = target_values;
+    input = input_values;
 
-        output_i = [hidden_o bias] * W_hidden_to_output;
-        output_o = activation(output_i);
+    bias = -ones(size(input,1), 1);
+
+    % forward pass
+    hidden_i = [input bias] * W_input_to_hidden;
+    hidden_o = activation(hidden_i);
+
+    output_i = [hidden_o bias] * W_hidden_to_output;
+    output_o = activation(output_i);
+
+    e = (target - output_o) .* (target - output_o) / 2;
+
+    disp(output_o)
+    disp(e)
     
-        e = (target - output_o) .* (target - output_o) / 2;
-        disp([num2str(output_o) ' ' num2str(e)]);
-     end
-
 end
+
 
 function [y] = activation(x)
     y = 1 ./ (1 + exp(-x));
