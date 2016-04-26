17786117312b8ebca917ffe9bcca0fb97efb15c2
a.vilkin <alexey.vilkin@apical.co.uk>
2016-04-26 09:45:46 +0100

    nn xor api example returned

---
+++ fc_learning_api_xor.m
@@ -0,0 +1,39 @@
+function [ output_args ] = fc_learning_api( input_args )
+%FC_LEARNING_API Summary of this function goes here
+%   Detailed explanation goes here
+
+    addpath('datasets');
+    addpath('layers');
+    addpath('nn');
+    
+    hidden_neurons_count = 2;
+    output_neurons_count = 1;
+    
+    [input_train, output_train, input_test, output_test] = GenerateDatasetXOR();
+    
+    rng(0,'v5uniform');
+
+    nn = network();
+    
+    nn.addLayer(LayerInput(2));
+    nn.addLayer(LayerFC(2,hidden_neurons_count));
+    nn.addLayer(LayerActivationSigmoid());
+    nn.addLayer(LayerFC(hidden_neurons_count,output_neurons_count));
+    nn.addLayer(LayerActivationSigmoid());
+    nn.addLayer(LossEuclidean(output_neurons_count));
+    
+    learning_rate = 5.0;
+        
+    for i = 1:2000
+        output_train_current = nn.forwardPropogate(input_train);
+        loss = nn.computeLoss(output_train_current, output_train);
+        nn.backPropagate(output_train_current, output_train, learning_rate);
+    end
+    
+    
+    output_test_current = nn.forwardPropogate(input_train);
+    
+    disp(output_test_current);
+    
+end
+
