b879e6bc3f1d06987f0bb6f45d0a0ae65bd0b2ae
a.vilkin <alexey.vilkin@apical.co.uk>
2016-05-13 13:16:32 +0100

    stub for triplet loss training

---
+++ training_funcs/trainTripletLossNetwork.m
@@ -0,0 +1,24 @@
+function [ output_args ] = trainTripletLossNetwork( nn, epochs, minibatchSize, margin, input_features, output_labels)
+    lossLayer = LossSoftmax(size(input_features,2));
+    
+    
+    for epoch = 1:epochs
+        itersCount = floor(size(output_labels,1)/minibatchSize);
+        
+        lossEpoch = [];
+        
+        for iters = 1:itersCount 
+            samples = input_features((iters-1)*minibatchSize+1:iters*minibatchSize,:);
+            answers = output_labels((iters-1)*minibatchSize+1:iters*minibatchSize,:);
+            
+            output_train_batch = nn.forwardPropogate(samples);
+            loss = lossLayer.computeLoss(output_train_batch{end}, answers);
+        
+            lossEpoch = [lossEpoch; loss];
+            nn.backPropagate(output_train_batch, lossLayer.computeDerivative(output_train_batch{end}, answers));
+        end
+
+        disp(['epoch : ' num2str(epoch) ' loss : ' num2str(sum(lossEpoch) / numel(lossEpoch))]);
+    end
+end
+
---
+++ triplet_learning.m
@@ -0,0 +1,39 @@
+function [ output_args ] = triplet_learning( input_args )
+%FC_LEARNING_API Summary of this function goes here
+%   Detailed explanation goes here
+    startup();
+    
+    [train_input, train_classes, test_input,  test_classes] = GenerateDatasetMNIST();
+    
+    hidden_neurons_count = 50;
+    input_dim = size(train_input,2);
+            
+    learningRate = 0.05;
+    momentum = 0.9;
+    weightDecay = 0.0005;
+    
+    nn = network();
+    
+    nn.addLayer(LayerInput(input_dim), {});
+    nn.addLayer(LayerFC(input_dim,hidden_neurons_count,WeightFillerGaussian(0.001)),  GradientUpdaterUsingMomentumAndWeightDecay(learningRate, momentum, weightDecay));
+    nn.addLayer(LayerActivationRELU,  {});
+            
+    minibatchSize = 64;
+    epochs = 50;
+    margin = 0.3;
+    
+    trainTripletLossNetwork(nn, epochs, minibatchSize, margin, train_input, train_classes);
+
+    %output_train_full = nn.forwardPropogate(train_input);
+    %[~, ind_train] = max(output_train_full{end}');
+    %[~, ind_train_gt] = max(train_classes');
+    %accuracy_train = (sum(ind_train == ind_train_gt)) / numel(ind_train);
+
+    %output_test_full = nn.forwardPropogate(test_input);
+    %[~, ind_test] = max(output_test_full{end}');
+    %[~, ind_test_gt] = max(test_classes');
+    %accuracy_test = (sum(ind_test == ind_test_gt)) / numel(ind_test);
+
+    %disp(['train accuracy : ' num2str(accuracy_train) ' test accuracy : ' num2str(accuracy_test)]);
+end
+
