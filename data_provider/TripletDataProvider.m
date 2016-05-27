b8ebab47a8df5245cbf73aa1272b32a1717da990
a.vilkin <alexey.vilkin@apical.co.uk>
2016-05-27 11:16:11 +0100

    variables renamed

--- data_provider/TripletDataProvider.m
+++ data_provider/TripletDataProvider.m
@@ -47,15 +47,15 @@ classdef TripletDataProvider < handle
             debug_log = 0;
 
             count = 0;
-            feat_shift = [];
-            feat_count = [];
+            label_shift = [];
+            label_count = [];
 
             feat_for_person_min = 100000000000;
             feat_for_person_max = -1;
 
             for i = 1:numel(obj.features)
-                feat_shift(end+1) = count;
-                feat_count(end+1) = numel(obj.features{i});
+                label_shift(end+1) = count;
+                label_count(end+1) = numel(obj.features{i});
                 count = count + numel(obj.features{i});
 
                 feat_for_person_min = min(feat_for_person_min, numel(obj.features{i}));
@@ -72,26 +72,26 @@ classdef TripletDataProvider < handle
                 disp(['person index check ' num2str(sum(positive_person_index == negative_person_index)==0)]);
             end
 
-            positive_sizes = feat_count(positive_person_index);
-            positive_shifts = feat_shift(positive_person_index);
+            positive_sizes = label_count(positive_person_index);
+            positive_shifts = label_shift(positive_person_index);
             positive_arr_index_1 = positive_shifts + rem(randi([0 feat_for_person_max],1,triplets_count), positive_sizes) + 1;
             positive_arr_index_2 = positive_shifts + rem(randi([0 feat_for_person_max],1,triplets_count), positive_sizes) + 1;
 
             if(debug_log)
 
-                disp(['arr index check 1 ' num2str(sum(positive_arr_index_1 > feat_shift(positive_person_index)) == triplets_count)]);
-                disp(['arr index check 2 ' num2str(sum(positive_arr_index_1 <= feat_shift(positive_person_index)+feat_count(positive_person_index)) == triplets_count)]);
-                disp(['arr index check 3 ' num2str(sum(positive_arr_index_2 > feat_shift(positive_person_index)) == triplets_count)]);
-                disp(['arr index check 4 ' num2str(sum(positive_arr_index_2 <= feat_shift(positive_person_index)+feat_count(positive_person_index)) == triplets_count)]);
+                disp(['arr index check 1 ' num2str(sum(positive_arr_index_1 > label_shift(positive_person_index)) == triplets_count)]);
+                disp(['arr index check 2 ' num2str(sum(positive_arr_index_1 <= label_shift(positive_person_index)+label_count(positive_person_index)) == triplets_count)]);
+                disp(['arr index check 3 ' num2str(sum(positive_arr_index_2 > label_shift(positive_person_index)) == triplets_count)]);
+                disp(['arr index check 4 ' num2str(sum(positive_arr_index_2 <= label_shift(positive_person_index)+label_count(positive_person_index)) == triplets_count)]);
             end
 
-            negative_sizes = feat_count(negative_person_index);
-            negative_shifts = feat_shift(negative_person_index);
+            negative_sizes = label_count(negative_person_index);
+            negative_shifts = label_shift(negative_person_index);
             negative_arr_index = negative_shifts + rem(randi([0 feat_for_person_max],1,triplets_count), negative_sizes) + 1;
 
             if(debug_log)
-                disp(['arr index check 5 ' num2str(sum(negative_arr_index > feat_shift(negative_person_index)) == triplets_count)]);
-                disp(['arr index check 6 ' num2str(sum(negative_arr_index <= feat_shift(negative_person_index)+feat_count(negative_person_index)) == triplets_count)]);
+                disp(['arr index check 5 ' num2str(sum(negative_arr_index > label_shift(negative_person_index)) == triplets_count)]);
+                disp(['arr index check 6 ' num2str(sum(negative_arr_index <= label_shift(negative_person_index)+label_count(negative_person_index)) == triplets_count)]);
             end
 
             triplets = [positive_arr_index_1' positive_arr_index_2' negative_arr_index'];
