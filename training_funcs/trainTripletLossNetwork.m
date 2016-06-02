7a65f5683f13a606cae80d30f2c540a1990cf02a
a.vilkin <alexey.vilkin@apical.co.uk>
2016-06-02 16:44:41 +0100

    added l2 normalisation to features(commented)

--- training_funcs/trainTripletLossNetwork.m
+++ training_funcs/trainTripletLossNetwork.m
@@ -1,32 +1,28 @@
 function [ output_args ] = trainTripletLossNetwork( nn, epochs, minibatchSize, margin, dataProvider)
-    
-    
     lossTriplet = LossTriplet(margin);
     
-    
-    
     for epoch = 1:epochs
-        
-    
         itersCount = floor(dataProvider.getSamplesCount()/minibatchSize);
-    
         lossEpoch = 0;
     
         for iters = 1:itersCount 
-        
             feats = struct();
             [feats.anchor, feats.positive, feats.negative] = dataProvider.getMinibatch(minibatchSize);
-    
+            
+            %feats.anchor =      bsxfun(@rdivide, feats.anchor,      sqrt(sum(abs(feats.anchor).^2,2)));
+            %feats.positive =    bsxfun(@rdivide, feats.positive,    sqrt(sum(abs(feats.positive).^2,2)));
+            %feats.negative =    bsxfun(@rdivide, feats.negative,    sqrt(sum(abs(feats.negative).^2,2)));
+            
+            %anchorNorm =    sqrt(sum(abs(feats.anchor).^2,2));
+            %positiveNorm =  sqrt(sum(abs(feats.positive).^2,2));
+            %negativeNorm =  sqrt(sum(abs(feats.negative).^2,2));
             anchorOutputs = nn.forwardPropogate(feats.anchor);
             positiveOutputs = nn.forwardPropogate(feats.positive);
             negativeOutputs = nn.forwardPropogate(feats.negative);
             
-            
             loss = lossTriplet.computeLoss(anchorOutputs{end}, positiveOutputs{end}, negativeOutputs{end});
-        
             [anchor_derivative, positive_derivative, negative_derivative] = lossTriplet.computeDerivative(anchorOutputs{end}, positiveOutputs{end}, negativeOutputs{end});
         
-            
             anchorGrads = nn.backPropagate1(anchorOutputs, anchor_derivative);
             positiveGrads = nn.backPropagate1(positiveOutputs, positive_derivative);
             negativeGrads = nn.backPropagate1(negativeOutputs, negative_derivative);
@@ -41,6 +37,7 @@ function [ output_args ] = trainTripletLossNetwork( nn, epochs, minibatchSize, m
             
             nn.updateWeights(grad);
             
+            %disp(['epoch : ' num2str( sum(loss) / numel(loss))]);
             lossEpoch = lossEpoch + sum(loss) / numel(loss);
         end
 
